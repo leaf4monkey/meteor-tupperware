dea8ac88002f50622a2bc34e8ba37dd393e56495
leaf4monkey <joc98@163.com>
2017-03-26 13:05:51 +0800

    修改启动脚本

--- run/startup.sh
+++ run/startup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-sh ./pre_build_env_setup.sh
+sh /scripts/pre_build_env_setup.sh
 
-sh ./post_build_env_setup.sh
+sh /scripts/post_build_env_setup.sh
 
 exec node /home/node/output/bundle/main.js "$@"
